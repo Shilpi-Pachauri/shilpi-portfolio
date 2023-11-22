4ffb678ac8ec5eb4a64a0538b93dc937d4b1dba9
Shilpi Pachauri <shilpipachauri068@gmail.com>
2023-11-23 00:17:13 +0530

    portfolio done

---
+++ src/data/constants.js
@@ -0,0 +1,192 @@
+export const Bio = {
+  name: "Shilpi Pachauri",
+  roles: ["Full Stack Developer", "Web Developer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Shilpi-Pachauri",
+  resume:
+    "https://drive.google.com/file/d/1H9JMn4DTEqmzF8mYDvA2VfwYMroSHxlJ/view?usp=sharing",
+  twitter: "https://twitter.com/shilpi_pachauri",
+  linkedin: "https://www.linkedin.com/in/shilpi-pachauri-75025a1a6/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Maria DB",
+        image:
+          "https://raw.githubusercontent.com/docker-library/docs/554e4b9aaac2e266b9ab31e9a312cb6f96d69286/mariadb/logo.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png",
+    role: "Laravel Developer",
+    company: "Intechno Pvt. Ltd.",
+    date: "May 2022 - Feb 2023",
+    desc: "Working on the Full Stack of the web application using Laravel, Javascript, and Bootstrap.",
+    skills: [
+      "Mysql",
+      "Php",
+      "Laravel",
+      "Html",
+      "Css",
+      "Bootstrap",
+      "Javascript",
+    ],
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw3KY9j2SE3NfLn3j6dd0CbHVXg8wUDZzPdmmlB8SuMyEtqTw1DreNoRoMyfr9kbeKnvs&usqp=CAU",
+    role: "Junior Software Developer",
+    company: "Khan Global Studies Pvt. Ltd.",
+    date: "March 2023 - Present",
+    desc: "Working on the backend of the web application using Laravel, MariaDB, and Vue.Js.",
+    skills: [
+      "Laravel",
+      "VueJs",
+      "Craftable",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bitbucket",
+      "MariaDB",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://images.collegedunia.com/public/college_data/images/logos/295px-I1GNOU_Logosvg.png",
+    school: "Indira Gandhi National Open University",
+    date: "Jan 2022 - Present",
+    grade: "70%",
+    desc: "I am currently pursuing a Bachelor's degree in Bachelor of Computer Applications at Indira Gandhi National Open University. I have completed 4 semesters.",
+    degree: "Bachelor of Technology - BCA, Bachelor of Computer Applications",
+  },
+  {
+    id: 1,
+    img: "https://nstihaldwani.dgt.gov.in/sites/default/files/2021-08/90Hi3zsH_400x400.jpg",
+    school: "National Skill Training Institute",
+    date: "Nov 2019 - March 2022",
+    grade: "85%",
+    desc: "I completed Advance Diplome in IT from National Skill Training Institue in Indore.",
+    degree: "Advance Diploma in IT",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2017/08/06/22/01/books-2596809_1280.jpg",
+    school: "Shree Krishna Inter College, Devkhera",
+    date: "July 2017 - Apr 2018",
+    grade: "71.2%",
+    desc: "I completed my class 12th Shree Krishna Inter College, Devkhera.",
+    degree: "Senior Secondary",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Online Exam management",
+    date: "May 2022 - Feb 2023",
+    description:
+      "Online Exam management is a web application designed to conduct student exams and enhance productivity in the workplace. It provides a user-friendly interface for users' Student can give exam.",
+    tags: ["Laravel", "Bootstrap ", "Php", "Javascript"],
+    category: "web application",
+    webapp: "https://onlineexammanagement.com/",
+  },
+  {
+    id: 0,
+    title: "Health Guider",
+    date: "Sept 2021 - Feb 2022",
+    description:
+      "This web application used for daily to daily prescriptions for health issues that can be recover health.",
+    tags: ["Laravel", "Bootstrap", "Php", "HTML", "CSS"],
+    category: "web application",
+    // webapp: "https://vexa-app.netlify.app/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
